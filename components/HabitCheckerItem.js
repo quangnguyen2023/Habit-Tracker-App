ec53961db18a51527c74f27d68db9e6be2f9d571
Quang <nmquang2012@gmail.com>
2023-07-02 17:20:25 +0700

    Add feature: add new Habit

--- components/HabitCheckerItem.js
+++ components/HabitCheckerItem.js
@@ -1,9 +1,9 @@
 import styles from './HabitCheckerItem.module.css';
 
-export default function HabitCheckerItem({ month }) {
-  const weekDays  = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+export default function HabitCheckerItem({ month, habitTitle }) {
+  const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-  const days = () => {
+  const days = (() => {
     let arr = [];
     let year = new Date().getFullYear();
     let lastDayPrevMonth = new Date(year, month, 0);
@@ -15,29 +15,31 @@ export default function HabitCheckerItem({ month }) {
     for (let i = 0; i < dayOfWeekFirstDay; i++) {
       arr.push('');
     }
-
+    // add the days of month
     for (let i = 1; i <= numberOfDays; i++) {
       arr.push(i);
     }
 
     return arr;
-  }
+  })();
 
-  return(
+  return (
     <div className={styles['habit-checker__item']}>
-      <h2> Ngoại Ngữ </h2>
+      <h2> {habitTitle} </h2>
       <div className={styles.calendar}>
         <div className={styles.weekdays}>
-          { weekDays.map((weekday, index) =>
-            <span className={styles.weekday} key={index}> {weekday} </span>)
-          }
+          {weekDays.map((weekday, index) => (
+            <span className={styles.weekday} key={index}>
+              {weekday}
+            </span>
+          ))}
         </div>
         <div className={styles.days}>
-          { days().map((day, index) =>
+          {days.map((day, index) => (
             <span className={`${day ? styles.day : ''}`} key={index}>
               {day}
             </span>
-          )}
+          ))}
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,129 @@
-import { useState } from "react";
-import Head from "next/head";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import InputLabel from "@mui/material/InputLabel";
+import { useState } from 'react';
+import Head from 'next/head';
+import Grid from '@mui/system/Unstable_Grid';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import { Box } from '@mui/material';
 
-import HabitCheckerItem from "../components/HabitCheckerItem";
-import styles from "../styles/HabitChecker.module.css";
+import HabitCheckerItem from '../components/HabitCheckerItem';
+import styles from '../styles/HabitChecker.module.css';
 
 export default function HabitChecker() {
   const [month, setMonth] = useState(new Date().getMonth());
+  const [titles, setTitles] = useState([]);
+  const [newTitle, setNewTitle] = useState('');
+  const [open, setOpen] = useState(false);
 
-  const handleChange = (event) => {
+  const toggleDialog = () => {
+    setOpen(!open);
+  };
+
+  const handleAddTitle = (e) => {
+    if (newTitle.trim() === '') return;
+    if (e.key && e.key !== 'Enter') return;
+
+    // prevent browser submit form when press Enter in input field (TextField)
+    e.preventDefault();
+
+    setTitles((prevState) => [...prevState, newTitle]);
+    setNewTitle('');
+    setOpen(false);
+  };
+
+  const handleChangeNewTitle = (e) => {
+    setNewTitle(e.target.value);
+  };
+
+  const handleChangeMonth = (event) => {
     setMonth(event.target.value);
   };
 
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Habit Checker App</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
+        <title>Habit Tracker App</title>
       </Head>
 
       <main>
-        <div className={styles["habit-checker"]}>
-          <FormControl fullWidth sx={{ mb: 4 }}>
-            <InputLabel id="demo-simple-select-label">Month</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={month}
-              label="Month"
-              onChange={handleChange}
-            >
-              <MenuItem value={0}> Tháng 1 </MenuItem>
-              <MenuItem value={1}> Tháng 2 </MenuItem>
-              <MenuItem value={2}> Tháng 3 </MenuItem>
-              <MenuItem value={3}> Tháng 4 </MenuItem>
-              <MenuItem value={4}> Tháng 5 </MenuItem>
-              <MenuItem value={5}> Tháng 6 </MenuItem>
-              <MenuItem value={6}> Tháng 7 </MenuItem>
-              <MenuItem value={7}> Tháng 8 </MenuItem>
-              <MenuItem value={8}> Tháng 9 </MenuItem>
-              <MenuItem value={9}> Tháng 10 </MenuItem>
-              <MenuItem value={10}> Tháng 11 </MenuItem>
-              <MenuItem value={11}> Tháng 12 </MenuItem>
-            </Select>
-          </FormControl>
-          <HabitCheckerItem month={month} />
+        <div className={styles['habit-checker']}>
+          <Grid container p={2}>
+            {/* Habits List */}
+            <Grid lg={10.5} md={10} sm={8}>
+              <Grid container spacing={8}>
+                {titles.map((title) => (
+                  <Grid xs={6}>
+                    <HabitCheckerItem month={month} habitTitle={title} />
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
+
+            {/* Actions */}
+            <Grid lg={1.5} md={2} sm={4} pl={2}>
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                {/* "Month" Select */}
+                <FormControl>
+                  <InputLabel id="demo-simple-select-label">Month</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={month}
+                    label="Month"
+                    onChange={handleChangeMonth}
+                  >
+                    <MenuItem value={0}> January </MenuItem>
+                    <MenuItem value={1}> February </MenuItem>
+                    <MenuItem value={2}> March </MenuItem>
+                    <MenuItem value={3}> April </MenuItem>
+                    <MenuItem value={4}> May </MenuItem>
+                    <MenuItem value={5}> June </MenuItem>
+                    <MenuItem value={6}> July </MenuItem>
+                    <MenuItem value={7}> August </MenuItem>
+                    <MenuItem value={8}> September </MenuItem>
+                    <MenuItem value={9}> October </MenuItem>
+                    <MenuItem value={10}> Novermber </MenuItem>
+                    <MenuItem value={11}> December </MenuItem>
+                  </Select>
+                </FormControl>
+
+                {/* "New Habit" button */}
+                <Button variant="outlined" onClick={toggleDialog} sx={{ marginTop: 2 }}>
+                  New Habit
+                </Button>
+                <Dialog open={open} onClose={toggleDialog} maxWidth="xs" fullWidth>
+                  <DialogTitle> Add New Habit </DialogTitle>
+                  <DialogContent>
+                    <TextField
+                      autoFocus
+                      margin="dense"
+                      id="name"
+                      label="Habit Title"
+                      type="text"
+                      fullWidth
+                      variant="standard"
+                      value={newTitle}
+                      onChange={handleChangeNewTitle}
+                      onKeyDown={handleAddTitle}
+                    />
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={toggleDialog}>Cancel</Button>
+                    <Button onClick={handleAddTitle}>Add</Button>
+                  </DialogActions>
+                </Dialog>
+              </Box>
+            </Grid>
+          </Grid>
         </div>
       </main>
-    </div>
+    </>
   );
 }
